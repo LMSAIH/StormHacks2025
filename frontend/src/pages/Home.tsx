3c169ab269b54220fe4252ed01d8b7692330561a
Axel Velasquez <yoloax11@gmail.com>
2025-10-04 13:02:57 -0700

    intiialized frontend

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+
+    return (
+        <div>
+            <Button>Hello </Button>
+            <h1>Home Page</h1>
+        </div>
+    )
+}
+
+
+export default Home;
