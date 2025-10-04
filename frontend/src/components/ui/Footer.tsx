0fcd067d899f83646a3c341bb2b69e3716138a5f
Elliott Schmechel <elliottschmechel@gmail.com>
2025-10-04 16:52:58 -0700

    Refactor Footer component layout for improved structure and readability

--- frontend/src/components/ui/Footer.tsx
+++ frontend/src/components/ui/Footer.tsx
@@ -5,22 +5,27 @@ const Footer: React.FC = () => {
         <footer className="bg-black opacity-40 text-white py-6 mt-auto">
             <div className="container mx-auto px-4">
                 <div className="flex flex-col md:flex-row justify-between items-center opacity-100">
-                    <div className="mb-4 md:mb-0">
+                    <div className="justify-start">
                         <p className="text-sm">
                             &copy; {new Date().getFullYear()} All rights reserved.
                         </p>
                     </div>
-                    <div className="flex space-x-6">
-                        <a href="#platform" className="text-sm hover:text-gray-300 transition-all">
-                            Platform
-                        </a>
-                        <a href="#solution" className="text-sm hover:text-gray-300 transition-all">
-                            Solution
-                        </a>
-                        <a href="#impact" className="text-sm hover:text-gray-300 transition-all">
-                            Impact
-                        </a>
+                    <div className="flex justify-center items-center text-center">
+                        <p className="text-sm">
+                            Written by Axel Velasquez, Diego Delgado, Elliott Schmechel, and Pawel Michalski for SFU StormHacks 2025
+                        </p>
                     </div>
+                    <div className="flex gap-4 justify-end pr-4">
+                    <a href="#platform" className="text-sm hover:text-gray-300 transition-all">
+                        Platform
+                    </a>
+                    <a href="#solution" className="text-sm hover:text-gray-300 transition-all">
+                        Solution
+                    </a>
+                    <a href="#impact" className="text-sm hover:text-gray-300 transition-all">
+                        Impact
+                    </a>
+                </div>
                 </div>
             </div>
         </footer>
